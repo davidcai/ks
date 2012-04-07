ab0257340f300c8eba558ad430040d388c1670c3
David Cai <w.david.cai@gmail.com>
2012-04-06 22:44:22 -0700

    assignment 4 first commit

---
+++ mwa2/assignment4/diary.js
@@ -0,0 +1,55 @@
+(function($) {
+
+  $('#page_list #btn_add').on('click', function(e) { add(); });
+
+  $('#page_add #btn_save').on('click', function(e) { save(); });
+
+  $('#page_add #btn_cancel').on('click', function(e) { cancel(); });
+
+  $('#page_list .entries article').on('click', function() { slide($(this)); });
+
+
+  function add() {
+    $('body').attr('data-mode', 'add');
+  }
+
+
+  function save() {
+    $('body').attr('data-mode', 'list');
+  }
+
+
+  function cancel() {
+    $('body').attr('data-mode', 'list');
+  }
+
+
+  function slide($article) {
+    var $wrapper = $('.content_w', $article);
+    var $content = $('.content', $wrapper);
+    var wrapperHeight = $wrapper.height();
+    var contentHeight = $content.outerHeight(true);
+
+    $article.toggleClass('open');
+    if ($article.hasClass('open')) {
+      setTimeout(function() {
+        $wrapper.addClass('transition').css('height', contentHeight);
+      }, 10);
+    }
+    else {
+      setTimeout(function() {
+        $wrapper./*removeClass('transition').*/css('height', wrapperHeight);
+        setTimeout(function() {
+          $wrapper.addClass('transition').css('height', 0);
+        }, 10);
+      }, 10);
+    }
+
+    $wrapper.one('transitionEnd webkitTransitionEnd transitionend oTransitionEnd msTransitionEnd', function() {
+      if ($article.hasClass('open')) {
+        $wrapper.removeClass('transition').css('height', 'auto');
+      }
+    });
+  }
+
+})(jQuery);
