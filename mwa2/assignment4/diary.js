9c01eb344cb7a980a01f637d7719793522a55050
David Cai <w.david.cai@gmail.com>
2012-04-07 01:28:05 -0700

    add geolocation

--- mwa2/assignment4/diary.js
+++ mwa2/assignment4/diary.js
@@ -11,16 +11,43 @@
 
   function add() {
     $('body').attr('data-mode', 'add');
+
+    if (navigator.geolocation) {
+      var $status = $('#geo_status');
+
+      function success(position) {
+        $status.text('Your location:');
+        $('#geo_coords').show();
+        $('#geo_coords_latitude').text('Latitude: ' + (position.coords.latitude || 'unknown'));
+        $('#geo_coords_longitude').text('Longitude: ' + (position.coords.longitude || 'unknown'));
+        $('#geo_coords_altitude').text('Altitude: ' + (position.coords.altitude || 'unknown'));
+      }
+
+      function error(msg) {
+        $status.text('Cannot locate your location');
+        $('#geo_coords').hide();
+      }
+
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
   }
 
 
   function save() {
     $('body').attr('data-mode', 'list');
+    clearNewEntry();
   }
 
 
   function cancel() {
     $('body').attr('data-mode', 'list');
+    clearNewEntry();
+  }
+
+
+  function clearNewEntry() {
+    $('#new_entry_title').val('');
+    $('#new_entry_body').val('');
   }
 
 
