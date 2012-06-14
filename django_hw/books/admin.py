f61ae48aee179f549f651e3b3db7df56fdb5c72a
David <w.david.cai@gmail.com>
2012-06-13 22:55:47 -0700

    django_hw 1st commit

---
+++ django_hw/books/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from mysite.books.models import Publisher, Author, Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publishers', 'publication_date')
+    list_filter = ('publication_date', )
+    date_hierarchy = 'publication_date'
+    ordering = ('-publication_date', )
+    fields = ('title', 'authors', 'publishers', 'publication_date')
+    filter_horizontal = ('authors', )
+    raw_id_fields = ('publishers', )
+
+admin.site.register(Publisher)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+
