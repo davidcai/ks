df5f4cffda97ca5ec4015157e152c42b204aaa3a
David Cai <w.david.cai@gmail.com>
2014-03-15 17:46:52 -0700

    added barchart

--- incplan_dashboard/js/app.js
+++ incplan_dashboard/js/app.js
@@ -1,3 +1,4 @@
+// FE.log shim
 var FE = {
   log: function(msg) {
     if (console && console.log) {
@@ -6,29 +7,60 @@ var FE = {
   }
 };
 
+
 angular.module('app', [])
+
+  // safeApply (https://coderwall.com/p/ngisma)
+  .factory('safeApply', ['$rootScope', function($rootScope) {
+    return function(scope, fn) {
+      var phase = $rootScope.$$phase;
+      if (phase == '$apply' || phase == '$digest') {
+        if (fn) {
+          scope.$eval(fn);
+        }
+      } else {
+        if (fn) {
+          scope.$apply(fn);
+        } else {
+          scope.$apply();
+        }
+      }
+    }
+  }])
+
 
   .controller('MainCtrl', [
 
-    'incomeSourceMap', 'savingSources', 'stackedBarchartConfigMap', 
-      '$scope', '$log',
+    'incomeSourceMap', 
+    'savingSources', 
+    'stackedBarchartConfigMap', 
+    '$scope', 
+    '$log',
 
-    function(incomeSourceMap, savingSources, stackedBarchartConfigMap, 
-      $scope, $log) {
+    function(
+      incomeSourceMap, 
+      savingSources, 
+      stackedBarchartConfigMap, 
+      $scope, 
+      $log) {
 
-    $scope.pctKey = '50'
-    $scope.barIndex = 3;
+      $scope.pctKey = '50'
+      $scope.barIndex = 3;
 
-    $scope.incomeSources = incomeSourceMap[$scope.pctKey][$scope.barIndex];
-    $scope.savingSources = savingSources;
-    $scope.barchartConfig = stackedBarchartConfigMap[$scope.pctKey];
+      $scope.incomeSources = incomeSourceMap[$scope.pctKey][$scope.barIndex];
+      $scope.savingSources = savingSources;
+      $scope.barchartConfig = stackedBarchartConfigMap[$scope.pctKey];
 
+      $scope.showGoal = false;
+      $scope.toggleGoal = function(show) {
+        $scope.showGoal = show;
+      }
 
-    $scope.setBarchartConfig = function(config) {
-      $scope.barchartConfig = stackedBarchartConfigMap["20"];
-      //$scope.incomeSources = incomeSourceMap[strPctKey][++nBarIndex];
-    };
+      $scope.setBarchartConfig = function(config) {
+        $scope.barchartConfig = stackedBarchartConfigMap["20"];
+        //$scope.incomeSources = incomeSourceMap[strPctKey][++nBarIndex];
+      };
 
-    $log.log($scope.barchartConfig);
+      // $log.log($scope.barchartConfig);
 
-  }]);
+    }]);
--- incplan_dashboard/js/directives/stacked-barchart.js
+++ incplan_dashboard/js/directives/stacked-barchart.js
@@ -1,19 +1,33 @@
 angular.module('app')
 
-  .directive('stackedBarchart', ['$log', function($log) {
+  .directive('stackedBarchart', ['safeApply', '$log', function(safeApply, $log) {
 
     return {
       restrict: 'AE', 
       
       scope: {
-        'config': '='
+        'config': '=', 
+        'barIndex': '=', 
+        'goalLine': '='
       }, 
 
       link: function(scope, element, attrs) {
 
         var id = attrs.id
           , width = parseInt(attrs.width, 10)
-          , height = parseInt(attrs.height, 10);
+          , height = parseInt(attrs.height, 10)
+          , barChart
+          , fnUnwatchGoalLine
+          , fnUnwatchBarIndex;
+        
+        scope.$watch(
+          function() { return scope.config; },
+          function(newVal, oldVal) {
+            $log.log(newVal + ' : ' + oldVal);
+
+            updateUi();
+          }
+        );
 
         //
         // Update UI
@@ -21,26 +35,58 @@ angular.module('app')
 
         function updateUi() {
 
+          if (barChart) {
+            barChart.destroy();
+            barChart = null;
+          }
+
           element.html('');
+
 
           YUI().use('fe-stacked-barchart', function(Y) {
 
-            var barChart = new Y.FE.StackedBarChart(id, width, height, scope.config);
+            barChart = new Y.FE.StackedBarChart(id, width, height, scope.config);
+            barChart.on('rendered', onRendered);
             barChart.render();
           });
         }
 
-        
-        scope.$watch(
-          
-          function() { return scope.config; },
 
-          function(newVal, oldVal) {
+        function onRendered() {
+
+          // Goal line
+
+          if (fnUnwatchGoalLine) {
+            fnUnwatchGoalLine();
+          }
+
+          fnUnwatchGoalLine = scope.$watch('goalLine', function(newVal, oldVal) {
             $log.log(newVal + ' : ' + oldVal);
 
-            updateUi();
+            if (scope.goalLine) {
+              barChart.showHorizontalLine(0);
+            }
+            else {
+              barChart.hideHorizontalLine(0); 
+            }
+          });
+
+          // Bar index
+
+          if (fnUnwatchBarIndex) {
+            fnUnwatchBarIndex();
           }
-        ); // end of $watch
+
+          fnUnwatchBarIndex = scope.$watch('barIndex', function(newVal, oldVal) {
+            $log.log(newVal + ' : ' + oldVal);
+
+            barChart.showBubble(scope.barIndex);
+          });
+
+
+          safeApply(scope);
+
+        } // end of onRendered
 
       } // end of link
     };
