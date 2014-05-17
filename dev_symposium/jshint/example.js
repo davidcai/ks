1dd860789713eb32fa97c10419bc6fb10ca833c5
David Cai <w.david.cai@gmail.com>
2014-05-17 01:43:06 -0700

    more jshint stuff

--- dev_symposium/jshint/example.js
+++ dev_symposium/jshint/example.js
@@ -1,14 +1,40 @@
-/* jshint curly: true, forin: true, undef: true, unused: false */
+/* jshint
+    curly: true,
+    forin: true,
+    latedef: nofunc,
+    plusplus: true,
+    undef: true,
+    unused: false,
+    laxcomma: true
+*/
 /* global console */
 
-function doSomething() {
-  var hello = '1',
-      map = {};
+function hello() {
 
-  if (hello) {
-    console.log('hello!');
-    for (var key in map) {
-      console.log(key);
-    }
+  test(hello);
+
+  function test(msg) {
+    $('#test').html(msg); // global
+    console.log(msg);
   }
+
+  var hello = 'David'; //latedef
+  if (hello)
+    console.log('hello!'); // curly
+
+  var map = {};
+  for (var key in map) { // forin
+    console.log(key);
+  }
+
+  var i = 0;
+  i--; // plusplus
+
+  var firstName = "David";
+  console.log(firstname); //undef
+
+  var a = 1
+    , b = 2; // laxcomma
 }
+
+var FE = {};
